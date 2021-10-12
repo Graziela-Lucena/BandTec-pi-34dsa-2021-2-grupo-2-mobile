48d505ba6e973ffa4b6792ec77b42e80a5b1dc3f
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-11 22:35:26 -0300

    adding error email page

---
+++ app/src/main/java/com/example/justgeek_base_app/MainErrorEmailLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.justgeek_base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainErrorEmailLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_error_email_login)
+    }
+}
