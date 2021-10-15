9f5169f57ef6b1fab367a3d773ca81e514aab7cd
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-15 20:34:03 -0300

    layout first step from tutorial first access

---
+++ app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccessFirstStepActivity.kt
@@ -0,0 +1,11 @@
+package com.example.justgeek_base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorialFirstAccessFirstStepActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial_first_access_first_step)
+    }
+}
