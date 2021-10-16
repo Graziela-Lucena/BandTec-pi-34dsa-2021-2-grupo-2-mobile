08d95f4e6bffdfd4ef4ef204454dcb2d9e762063
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-15 22:36:29 -0300

    adding functions to change screen

--- app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccessSecondStepActivity.kt
+++ app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccessSecondStepActivity.kt
@@ -1,11 +1,23 @@
 package com.example.justgeek_base_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TutorialFirstAccessSecondStepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial_first_access_second_step)
+    }
+
+    fun backStepOne(v:View) {
+        startActivity(Intent(this,
+            TutorialFirstAccessFirstStepActivity::class.java))
+    }
+
+    fun nextStepThree(v:View) {
+        startActivity(Intent(this,
+            TutorialFirstAccessThirdStepActivity::class.java))
     }
 }
--- app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccestEndedPageActivity.kt
+++ app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccestEndedPageActivity.kt
@@ -1,11 +1,18 @@
 package com.example.justgeek_base_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class TutorialFirstAccestEndedPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial_first_accest_ended_page)
+    }
+
+    fun backToThirdStep(v:View) {
+        startActivity(Intent(this,
+            TutorialFirstAccessThirdStepActivity::class.java))
     }
 }
