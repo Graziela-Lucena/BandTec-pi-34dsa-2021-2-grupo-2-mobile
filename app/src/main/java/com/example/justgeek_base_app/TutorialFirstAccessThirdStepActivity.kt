ac9ffaf6ce682337b17f24b262bb616ff565b385
taiprogrammer <taiza.marques@bandtec.com.br>
2021-10-15 21:16:48 -0300

    third page from tutorial first access

---
+++ app/src/main/java/com/example/justgeek_base_app/TutorialFirstAccessThirdStepActivity.kt
@@ -0,0 +1,11 @@
+package com.example.justgeek_base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TutorialFirstAccessThirdStepActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial_first_access_third_step)
+    }
+}
