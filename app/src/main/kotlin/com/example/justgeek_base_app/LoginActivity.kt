324a4045819033e311fa4e62c5100c09beddab0e
Graziela-Lucena <graziela.lima@bandtec.com.br>
2021-10-26 18:53:47 -0300

    app funcionando - login, cadastro

--- app/src/main/kotlin/com/example/justgeek_base_app/LoginActivity.kt
+++ app/src/main/kotlin/com/example/justgeek_base_app/LoginActivity.kt
@@ -3,12 +3,17 @@ package com.example.justgeek_base_app
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextWatcher
-import android.util.Log
-import android.widget.TextView
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
+import com.example.justgeek.api.ApiInterface
+import com.example.justgeek.api.RetrofitInstance
+import com.example.justgeek.models.SignInBody
+import okhttp3.ResponseBody
+import retrofit2.Response
 
 class LoginActivity: AppCompatActivity(R.layout.activity_main_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +28,37 @@ class LoginActivity: AppCompatActivity(R.layout.activity_main_login) {
             startActivity(intent)
         }
 
-        singInButton.setOnClickListener {
-            val intent = Intent(this, HomePageActivity::class.java)
-            startActivity(intent)
-        }
+        singInButton.setOnClickListener { v -> logged(v) }
+
+
+    }
+
+    fun logged(v: View){
+        val inputEmail: AppCompatEditText = findViewById(R.id.input_email)
+        val inputSenha: AppCompatEditText = findViewById(R.id.input_password)
+
+        signin(inputEmail.text.toString(),inputSenha.text.toString())
+    }
+
+    private fun signin(email: String, password: String){
+        val retIn = RetrofitInstance.getRetrofitInstance().create(ApiInterface::class.java)
+        val signInInfo = SignInBody(email, password)
+        retIn.signin(signInInfo).enqueue(object : retrofit2.Callback<ResponseBody> {
+            override fun onFailure(call: retrofit2.Call<ResponseBody>, t: Throwable) {
+                Toast.makeText(
+                    this@LoginActivity,
+                    t.message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onResponse(call: retrofit2.Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.code() == 200) {
+                    Toast.makeText(this@LoginActivity, "Login success!", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this@LoginActivity, TutorialAccessFirst::class.java))
+                } else {
+                    Toast.makeText(this@LoginActivity, "Login failed!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
     }
 }
