e20cc703ca4db7cbafc421cb082f85969a2c4ba8
taiprogrammer <taiza.marques@bandtec.com.br>
2021-11-03 21:36:36 -0300

    adding recycler view addresses from account configuration page

--- app/src/main/kotlin/com/example/justgeek_base_app/AccountConfigurationActivity.kt
+++ app/src/main/kotlin/com/example/justgeek_base_app/AccountConfigurationActivity.kt
@@ -7,5 +7,10 @@ class AccountConfigurationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_configuration)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val address = UserAddressFragment()
+        fragmentTransaction.add(R.id.fragment_address, address)
+        fragmentTransaction.commit()
     }
 }
