999a0792f703e253bf1a10ee050729b280968bec
taiprogrammer <taiza.marques@bandtec.com.br>
2021-11-11 14:26:14 -0300

    resolvind error on Manifest and linking homepage with cart

--- app/src/main/kotlin/com/example/justgeek_base_app/MyCartActivity.kt
+++ app/src/main/kotlin/com/example/justgeek_base_app/MyCartActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toolbar
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.res.ResourcesCompat
 
@@ -13,7 +14,7 @@ class MyCartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_cart)
 
-        val buttonPP = findViewById<AppCompatTextView>(R.id.button_pp_size)
+        
         val fragmentTransaction = supportFragmentManager.beginTransaction()
         val fragmentTransactionPromotion = supportFragmentManager.beginTransaction()
         val itemsCart = MyCartFragment()
