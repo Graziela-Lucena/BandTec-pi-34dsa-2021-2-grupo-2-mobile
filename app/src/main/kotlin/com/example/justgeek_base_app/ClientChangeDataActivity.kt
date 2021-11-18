7d4a474c4715378aef311d9bfb339f2553e301d7
gisele-araujo <gisele.flor@bandtec.com.br>
2021-11-17 22:10:11 -0300

    feat(alterar dados pessoais) configurações

---
+++ app/src/main/kotlin/com/example/justgeek_base_app/ClientChangeDataActivity.kt
@@ -0,0 +1,12 @@
+package com.example.justgeek_base_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.justgeek_base_app.R
+
+class ClientChangeDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_change_data)
+    }
+}
