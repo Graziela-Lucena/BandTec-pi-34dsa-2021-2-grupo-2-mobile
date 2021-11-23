3bcf4a3a504765c7ffdb0ff13261eb20ce96a3ef
Graziela-Lucena <graziela.lima@bandtec.com.br>
2021-11-23 19:19:26 -0300

    adding comment integration

---
+++ app/src/main/kotlin/com/example/justgeek_base_app/api/CommentApi.kt
@@ -0,0 +1,11 @@
+package com.example.justgeek_base_app.api
+
+import com.example.justgeek_base_app.data.CommentItem
+import retrofit2.http.GET
+
+interface CommentApi{
+    @GET("/rating")
+    suspend fun getHomeComments(): List<CommentItem>
+    @GET("/rating/{idProduct}")
+    suspend fun getProductComments(idProduct: Int): List<CommentItem>
+}
